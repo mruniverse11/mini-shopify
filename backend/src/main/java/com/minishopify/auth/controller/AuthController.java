006b9ca5ace307fe623010b5c2317222fc7c719d
Mrunal Adhav <104316129+mruniverse11@users.noreply.github.com>
2026-07-05 20:54:32 +0100

    Added validation to New registration

--- backend/src/main/java/com/minishopify/auth/controller/AuthController.java
+++ backend/src/main/java/com/minishopify/auth/controller/AuthController.java
@@ -3,6 +3,8 @@ package com.minishopify.auth.controller;
 import com.minishopify.auth.dto.RegisterRequest;
 import com.minishopify.auth.dto.RegisterResponse;
 import com.minishopify.auth.service.AuthService;
+
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,7 +16,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public RegisterResponse register(@RequestBody RegisterRequest request) {
+    public RegisterResponse register(@Valid @RequestBody RegisterRequest request) {
         return authService.register(request);
     }
 }
--- backend/src/main/java/com/minishopify/auth/dto/RegisterRequest.java
+++ backend/src/main/java/com/minishopify/auth/dto/RegisterRequest.java
@@ -1,10 +1,22 @@
 package com.minishopify.auth.dto;
 
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 @Data
 
 public class RegisterRequest {
+    @NotBlank(message = "Full name is required")
     private String fullName;
+    
+    @Email(message = "Email is invalid")
+    @NotBlank(message = "Email is required")
     private String email;
+    
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @NotBlank(message = "Password is required")
     private String password;
 }
